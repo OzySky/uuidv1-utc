5bb9d300b56e770a2b010ba0e83293cafb8f4a50
Ozy Design & Software <ozydesign1@gmail.com>
2018-10-05 12:24:56 +0300

    Change to emit UTC-based uuidv1

--- uuidv1.js
+++ uuidv1.js
@@ -2,8 +2,7 @@
 
 (function (globalCounter, slice) {
   function $ () {
-    var now = new Date()
-    var nowHex = (1e16 + (1e4 * (+new Date(now.getTime() - now.getTimezoneOffset() * 6e4) + 122192928e5)).toString(16))[slice](-16)
+    var nowHex = (1e16 + (1e4 * (+new Date() + 122192928e5)).toString(16))[slice](-16)
     globalCounter %= 4096
     return [nowHex[slice](8, 16), '-', nowHex[slice](4, 8), -1, nowHex[slice](1, 4), -8, (1e3 + (globalCounter++).toString(16))[slice](-3), '-']
       .concat(('' + 1e11).split('').map(function () { return Math.random().toString(16)[2] })).join('')
